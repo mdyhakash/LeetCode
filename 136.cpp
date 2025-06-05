a8fd9b0f1bfd946dc1069274e4ab943ec8bb5023
mdyhakash <mdyhakash@gmail.com>
2025-06-05 12:00:09 +0600

    Solved Leetcode 136

---
+++ 136.cpp
@@ -0,0 +1,15 @@
+//136. Single Number
+
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        int res = 0;
+
+        for(int val: nums){
+            res ^= val;
+        }
+
+        return res;
+        
+    }
+};
