d92e1f731f90301f79118f20b07d2bdfc7999a64
mdyhakash <mdyhakash@gmail.com>
2025-06-05 12:02:37 +0600

    Solved Leetcode 169

---
+++ 169.cpp
@@ -0,0 +1,22 @@
+// 169. Majority Element
+
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+    int freq = 0, ans = 0;
+
+    for(int i=0; i<nums.size(); i++){
+        if (freq==0){
+            ans = nums[i];
+
+        }
+        if(ans==nums[i]){
+            freq++;
+        }
+        else {
+            freq--;
+        }
+    }
+    return ans;
+    }
+};
